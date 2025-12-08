728e0ed85902c13cce9bb89f7a3d11b11c50e52a
Manik <beingdeveloper84@gmail.com>
2025-12-08 19:20:13 +0530

    updated preview

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-if (import.meta.env.DEV) {
-  const origError = console.error
-  console.error = (...args) => {
-    const msg = args && args[0] ? String(args[0]) : ''
-    if (msg.includes('net::ERR_ABORTED')) return
-    if (msg.includes('blob:http://')) return
-    origError(...args)
-  }
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
